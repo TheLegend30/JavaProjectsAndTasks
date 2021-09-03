6515c58869dccdf08d5d89c5612fffd48bb47e8b
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-03 18:02:54 +0300

    Create  Convert a string to an array.java
    Write a function to split a string and convert it into an array of words. For example:

---
+++ 8level/Convert a string to an array.java
@@ -0,0 +1,16 @@
+package katatemplateproject;
+import java.util.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+    }
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class Solution {
+
+    public static String[] stringToArray(String s) {
+       return s.split(" ");
+    }
+
+}
