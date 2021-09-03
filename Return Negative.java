9cde6149cd7a8f52aa0a6b3d74afe6627abc2ae8
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-03 16:23:42 +0300

    Create Return Negative.java
    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?

---
+++ Return Negative.java
@@ -0,0 +1,19 @@
+package katatemplateproject;
+
+public class KataTemplateProject {
+    public static void main(String[] args) {
+    }
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class Kata {
+
+  public static int makeNegative(final int x) {
+    
+    if(x>=0)return -x;
+    else return x;
+    
+  }
+  
+}
