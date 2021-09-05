00c6839ef390e016c3538579372a386e23983dfa
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-05 18:37:16 +0300

    Create Keep Hydrated!.java
    Nathan loves cycling.
    
    Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
    
    You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.

---
+++ 8level/Keep Hydrated!.java
@@ -0,0 +1,19 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+    }
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class KeepHydrated  {
+
+  public int Liters(double time)  {
+ 
+    return (int)time/2;
+  
+  }
+
+}
