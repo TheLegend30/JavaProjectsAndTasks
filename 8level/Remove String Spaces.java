0717dd45e25d0e2a817ad98e7aef61dd88c23914
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-05 12:48:57 +0300

    Create Remove String Spaces.java
    Simple, remove the spaces from the string, then return the resultant string.

---
+++ 8level/Remove String Spaces.java
@@ -0,0 +1,15 @@
+package katatemplateproject;
+import java.util.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+
+    }
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class Kata {
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
