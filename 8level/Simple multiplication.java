c99c2e053bc1427ee6147b3984f081e0c56ca51a
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-06 09:49:44 +0300

    Create  Simple multiplication.java
    This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.

---
+++ 8level/Simple multiplication.java
@@ -0,0 +1,21 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+
+    }
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class Sid {
+    public static int simpleMultiplication(int n) {
+        if(n%2==0){
+            return n*8;
+        }
+        else{
+            return n*9;
+        }
+    }
+}
