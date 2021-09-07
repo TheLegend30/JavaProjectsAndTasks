c169aa3c9a7848d6a7e5bc62db4b21f6b55f08b2
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-07 13:45:24 +0300

    Create  Beginner Series #4 Cockroach.java
    Description:
    The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
    
    For example:
    
    1.08 --> 30
    Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.

---
+++ 8level/Beginner Series #4 Cockroach.java
@@ -0,0 +1,16 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+    }
+
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class Cockroach{
+  public int cockroachSpeed(double x){
+    return (int)(x/36*1000);
+  }
+}
