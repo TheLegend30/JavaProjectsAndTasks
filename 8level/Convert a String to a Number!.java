6be5a8e50421077f1d5318686bd276ae077e442f
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-07 13:14:53 +0300

    Create  Convert a String to a Number!.java

---
+++ 8level/Convert a String to a Number!.java
@@ -0,0 +1,16 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+    }
+
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class StringToNumber {
+  public static int stringToNumber(String str) {
+    return Integer.parseInt(str);
+  }
+}
