d0482e4bed95f391e836998891c7c0bf2e3cc748
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-09 21:48:47 +0300

    Create  Keep up the hoop.java
    Description:
    Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
    
    Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
    
    If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
    If he doesn't get 10 hoops, return the string "Keep at it until you get it".

---
+++ 8level/Keep up the hoop.java
@@ -0,0 +1,20 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+        
+    }
+
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class HelpAlex{
+  public static String hoopCount(int n){
+   if(n<10){
+      return "Keep at it until you get it"; 
+   }   
+   return "Great, now move on to tricks";
+  }
+}
