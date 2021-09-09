e9194adda2cb577147b1eec7915167f8dff7a803
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-09 21:35:58 +0300

    Create Convert to Binary.java
    Task Overview
    Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
    
    to_binary(1)  /* should return 1 */
    to_binary(5)  /* should return 101 */
    to_binary(11) /* should return 1011 */

---
+++ 8level/Convert to Binary.java
@@ -0,0 +1,19 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+        
+    }
+
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+}
+class Kata {
+
+  public static int toBinary(int n) {
+    return Integer.parseInt(Integer.toBinaryString(n));
+  }
+
+}
