d1414e79b36b42861e0363d9081522c8b0c2dcb2
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-15 15:30:09 +0300

    Create  Grasshopper - Terminal game move function.java
    Description:
    Terminal game move function
    In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
    
    Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
    
    Example:
    move(3, 6) should equal 15

---
+++ 8level/Grasshopper - Terminal game move function.java
@@ -0,0 +1,19 @@
+package katatemplateproject;
+import java.util.*;
+import java.util.regex.*;
+import java.lang.StringBuilder;
+public class KataTemplateProject {
+    public static void main(String[] args) {
+        
+    }
+
+    public static <T> void prnt(T args){
+        System.out.println(args);
+    } 
+    
+}
+class Move {
+    public static int move(int position, int roll) {
+        return position+roll*2;
+    }
+}
