b2c33637f54f07c93872f5c2569b4464f0290af3
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-21 16:30:06 +0300

    Create Function 3 - multiplying two numbers.java
    Description:
    Implement a function which multiplies two numbers.

---
+++ 8level/Function 3 - multiplying two numbers.java
@@ -0,0 +1,14 @@
+package katatemplateproject;
+
+import java.util.*;
+public class KataTemplateProject
+{
+    public static void main(String[] args) {
+    }
+}
+
+class Kata {
+    public static int multiply(int num1, int num2) {
+        return num1*num2;
+    } 
+}
