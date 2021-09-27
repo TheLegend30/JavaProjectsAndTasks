6441a7f0c618b3a43d8eb6f5a294ccd64fcc751e
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-28 00:28:16 +0300

    Create Volume of a Cuboid.java
    Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.

---
+++ 8level/Volume of a Cuboid.java
@@ -0,0 +1,17 @@
+package katatemplateproject;
+
+import java.util.*;
+
+public class KataTemplateProject {
+
+    public static void main(String[] args) {
+    }
+
+}
+class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    return (length*width*height);
+  }
+  
+}
