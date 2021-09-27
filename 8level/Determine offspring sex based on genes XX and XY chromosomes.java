90ab28b931df9eebc1aa2d2b9b8620e2556ebd87
Oleh <57060917+TheLegend30@users.noreply.github.com>
2021-09-28 01:09:11 +0300

    Create  Determine offspring sex based on genes XX and XY chromosomes.java
    Description:
    The male gametes or sperm cells in humans and other mammals are heterogametic and contain one of two types of sex chromosomes. They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homogametic.
    
    The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
    
    Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
    
    If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";

---
+++ 8level/Determine offspring sex based on genes XX and XY chromosomes.java
@@ -0,0 +1,16 @@
+package katatemplateproject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class KataTemplateProject {
+
+    public static void main(String[] args) {
+    }
+
+}
+class Kata {
+  public static String chromosomeCheck(String sperm) {
+     return (sperm=="XX") ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son.";
+  }
+}
